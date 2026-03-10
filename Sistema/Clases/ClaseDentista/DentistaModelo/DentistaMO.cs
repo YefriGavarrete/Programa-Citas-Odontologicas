3bbd82f864cb88de7bd5bdb910e3200ef1687356
Erasmo Mejia <erasmomejia094@gmail.com>
2026-03-09 22:11:27 -0600

    a ver

---
+++ Sistema/Clases/ClaseDentista/DentistaModelo/DentistaMO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Clases.ClaseDentista.DentistaModelo
+{
+    internal class DentistaMO
+    {
+        public int Id_Especialidad { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Estado { get; set; }
+        public string Especialidad { get; set; }
+    }
+}
---
+++ Sistema/Clases/ClaseEspecialidad/EspecialidadMO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Clases.ClaseEspecialidad
+{
+    internal class EspecialidadMO
+    {
+        public int Id_Especialidad {  get; set; }
+        public string Especialidad { get; set; }
+        public string Estado { get; set; }
+    }
+}
